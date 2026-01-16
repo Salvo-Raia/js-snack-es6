4354becc432c859680190b1c0bad45a8e4a344a4
Salvo-Raia <salvo.raia@outlook.it>
2026-01-16 14:37:19 +0100

    Add Interval to simulate matches

--- js/script.js
+++ js/script.js
@@ -26,4 +26,41 @@ const soccerTeams = [
     {Name:"HTML FC", Points: 0, Fouls: 0}
 ]
 
+console.log("Vi presentiamo le squadre del CVR: Campionato virtuale randomico!");
+console.table(soccerTeams)
+console.log("Le regole sono semplici: ogni 2 secondi simuleremo i risultati delle partite giocate dalle squadre. Per ogni girone di partite ti mostreremo il risultati di punti e i falli cumulati dalle squadre fin a quel momento");
+
+let oneSecond = 1000;
+let partita = 0;
+const simulazione = setInterval(function() {
+    
+    for (let i = 0; i < soccerTeams.length; i++) {
+        soccerTeams[i].Points += randomNumbers(1, 8); 
+        soccerTeams[i].Fouls += randomNumbers(1,3); 
+    }
+    partita++;
+    console.log("Girone partite N°:", partita);
+    console.table(soccerTeams)
+
+    if (partita === 5) {
+        clearInterval(simulazione)
+    }
+
+}, oneSecond * 3)
+
+
+
+
+/* Funzioni */
+/**
+ * This function generates a random number between min and max value (both min and max values included).
+ * @param {number} min The minimum value
+ * @param {number} max The maximum value
+ * @returns {number} a random number between min and max values
+ */
+function randomNumbers (min, max) {
+    return Math.floor(Math.random () * (max-min + 1) + min);
+    
+}
+
 
