f1485655c4461849e4187303f4f5cb7a4dfee013
Salvo-Raia <salvo.raia@outlook.it>
2026-01-16 13:24:29 +0100

    Init repository - Set bike array of objects

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Snack 1 //
+const bikeExpo = [
+    {Name: "Furious", Peso: 20},
+    {Name: "Light", Peso: 15},
+    {Name: "Strong", Peso: 35}, 
+    {Name: "Kid", Peso: 10},
+    {Name: "Sport", Peso: 18},
+    {Name: "Off-Road", Peso: 22}
+]
