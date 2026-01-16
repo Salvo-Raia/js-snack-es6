1e2e2928a7fbd6f0cafb6671cb00b4632a7e8d2d
Salvo-Raia <salvo.raia@outlook.it>
2026-01-16 13:45:13 +0100

    Snack 2 - Add football teams array of objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // Snack 1 //
+console.log("%cSnack 1", "background: black; color: white; font-size: 20px");
+
 const bikeExpo = [
     {Name: "Furious", Peso: 20},
     {Name: "Light", Peso: 15},
@@ -11,4 +13,17 @@ const bikeExpo = [
 console.log("La bici più leggera è la nostra fantastica", bikeExpo[3].Name, "dal peso di", bikeExpo[3].Peso, "Kg");
 console.log("Vuoi dare un'occhiata alle altre?");
 console.table(bikeExpo)
+// TODO: Voglio provare una soluzione dinamica. Se non conosco i pesi e voglio una funzione che trovi da sola la più leggera?!
+
+// Snack 2 //
+console.log("%cSnack 2", "background: black; color: white; font-size: 20px");
+
+const soccerTeams = [
+    {Name:"Boolean United", Points: 0, Fouls: 0},
+    {Name:"Atletica Truthy", Points: 0, Fouls: 0},
+    {Name:"Saint CSS", Points: 0, Fouls: 0},
+    {Name:"Club Invokers", Points: 0, Fouls: 0},
+    {Name:"HTML FC", Points: 0, Fouls: 0}
+]
+
 
